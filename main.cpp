10e47fef81562741de7b65297e80634f1f2dd5f7
zhengsong <741633710@qq.com>
2018-04-03 18:30:29 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include "mainwindow.h"
 #include "login.h"
 #include <QApplication>
+#include <QSqlDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    qDebug() << "Available drivers:";
+    QStringList drivers = QSqlDatabase::drivers();
+    foreach(QString driver, drivers)
+       qDebug() << driver;
 
-    LOGINWINDOW loginwd;
-    loginwd.show();
+//    LOGINWINDOW loginwd;
+//    loginwd.show();
 
 //    MainWindow w;
 //    w.setMinimumSize(600,400);
